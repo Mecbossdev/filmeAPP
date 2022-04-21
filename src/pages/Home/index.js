ef8517478cfbc5bf0843bafe167b7b2b32a58ac2
Your Name <fernandesjeferson539@gmail.com>
2022-04-21 10:53:15 -0300

    acerto de erros de consumo
    consumo de API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,27 +3,31 @@ import { Container, ContainerCarousel, ContainerList } from './style';
 
 import Carousel from './Carousel';
 import List from '../../components/List';
-import { GetFilmsNowPlaying, GetFilmsTrending, GetGenders } from '../../services/filmsAPI';
+import { 
+  GetFilmsNowPlaying, 
+  GetFilmsTrending, 
+  GetGenders } 
+from '../../services/filmsAPI';
 
 const Home = () => {
 
   const [ListTrending, setListTrending] = useState([]);
-  const [ListFilmNow, setListFilmNow] = useState([]);
+  const [ListFilmNow, setListFilm] = useState([]);
   const [genders, setGenders] = useState([]);
 
   const getFilmsTrending = async (page) => {
     const response = await GetFilmsTrending(1);
     setListTrending(response.data.results);
   };
-  
+
   const getFilmsNowPlay = async (page) => {
     const response = await GetFilmsNowPlaying(1);
-    setListTrending(response.data.results);
+    setListFilm(response.data.results);
   };
 
   const getFilmsByGenders = async () => {
     const response = await GetGenders();
-    console.log(response.data);
+    setGenders(response.data.genres);
   };
 
   useEffect(() => {
@@ -54,12 +58,14 @@ const Home = () => {
       {
         genders.map(item => {
           const title = `Filmes de ${item.name}`;
-          <ContainerList key={`${item.id}`}>
-            <List
-              list={ListTrending}
-              title={title}
-            />
-          </ContainerList>
+          return (
+            <ContainerList key={`${item.id}`}>
+              <List
+                list={ListTrending}
+                title={title}
+              />
+            </ContainerList>
+          )
         })
       }
     </Container >
--- src/services/filmsAPI.js
+++ src/services/filmsAPI.js
@@ -4,17 +4,17 @@ const apiKey = 'api_key=c2e78b4a8c14e65dd6e27504e6df95ad';
 const language = 'Language=pt-BR';
 
 export const GetFilmsTrending = (page) => {
-  const url = `https://api.themoviedb.org/3/trending/movie/week?${apiKey}&${language}&page=${page}`;
+  const url = `trending/movie/week?${apiKey}&${language}&page=${page}`;
   return api.get(url);
 };
 
 export const GetFilmsNowPlaying = (page) => {
-  const url = `https://api.themoviedb.org/3/movie/now_playing?${apiKey}&${language}&page=${page}`;
+  const url = `movie/now_playing?${apiKey}&${language}&page=${page}`;
   return api.get(url);
 };
 
 export const GetGenders = () => {
-  const url = `https://api.themoviedb.org/3/genre/movie/list?${apiKey}&${language}`;
+  const url = `genre/movie/list?${apiKey}&${language}`;
   return api.get(url);
 };
 
